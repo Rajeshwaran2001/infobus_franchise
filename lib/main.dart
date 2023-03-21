85e5a244a26f4669fcec2b14de84c9ba09bd046c
Rajeshwaran <54212165+Rajeshwaran2001@users.noreply.github.com>
2023-03-21 23:41:07 +0530

    permission added

--- lib/main.dart
+++ lib/main.dart
@@ -4,6 +4,7 @@
 
 import 'package:flutter/material.dart';
 import 'package:webview_flutter/webview_flutter.dart';
+import 'package:permission_handler/permission_handler.dart';
 import 'src/navigation_controls.dart';
 import 'src/web_view_stack.dart';
 
@@ -17,23 +18,45 @@ void main() {
 }
 
 class WebViewApp extends StatefulWidget {
-  const WebViewApp({super.key});
+  const WebViewApp({Key? key}) : super(key: key);
 
   @override
   State<WebViewApp> createState() => _WebViewAppState();
 }
 
 class _WebViewAppState extends State<WebViewApp> {
-  late final WebViewController controller;
+  WebViewController? controller;
 
   @override
   void initState() {
     super.initState();
-    controller = WebViewController()
-      ..loadRequest(
-        Uri.parse('https://test.infobus.in/login'),
-      );
+    WidgetsBinding.instance?.addPostFrameCallback((_) => requestStoragePermission());
   }
+
+  Future<void> requestStoragePermission() async {
+    final PermissionStatus status = await Permission.storage.request();
+    if (status != PermissionStatus.granted) {
+      // If the user does not grant permission, show a dialog or toast message
+      // explaining why storage permission is needed and ask the user to grant
+      // permission again.
+    } else {
+    // Permission granted, request the write external storage permission
+    final PermissionStatus status = await Permission.storage.request();
+    if (status != PermissionStatus.granted) {
+      // If the user does not grant permission, show a dialog or toast message
+      // explaining why write external storage permission is needed and ask
+      // the user to grant permission again.
+    } else {
+      // Permission granted, load the web view
+      setState(() {
+        controller = WebViewController(); // Initialize the WebViewController object
+        controller?.loadRequest(
+          Uri.parse('https://test.infobus.in/login'),
+        );
+      });
+    }
+  }
+}
 
   @override
   Widget build(BuildContext context) {
@@ -41,10 +64,10 @@ class _WebViewAppState extends State<WebViewApp> {
       appBar: AppBar(
         title: const Text('InfoBus Login App'),
         actions: [
-          NavigationControls(controller: controller),
+          if (controller != null) NavigationControls(controller: controller!),
         ],
       ),
-      body: WebViewStack(controller: controller),
+      body: controller != null ? WebViewStack(controller: controller!) : const SizedBox(),
     );
   }
 }
