427c2b8862d364abe67038ad2c6f1152a428b820
Rajeshwaran <54212165+Rajeshwaran2001@users.noreply.github.com>
2023-07-17 17:58:08 +0530

    unwanted files removed

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,6 @@
-// Copyright 2022 The Flutter Authors. All rights reserved.
-// Use of this source code is governed by a BSD-style license that can be
-// found in the LICENSE file.
-
 import 'package:flutter/material.dart';
 import 'package:webview_flutter/webview_flutter.dart';
 import 'src/navigation_controls.dart';
-import 'src/web_view_stack.dart';
 
 void main() {
   runApp(
